fe7d529f3d119f98417ab091e580b5aca18010f2
Nicole Forseth <forseth.nico@gmail.com>
2022-08-23 20:59:25 -0600

    Add game start and prepare game methods

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,14 @@
+const Card = require('./Card');
 const data = require('./data');
+const Deck = require('./Deck');
+const Round = require('./Round');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -13,6 +18,21 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
+
+  createGameDeck(cardData) {
+    let cards = cardData.map(card => {
+      return new Card(card.id, card.question, card.answers, card.correctAnswer)
+    }) 
+    let deck = new Deck(cards)
+    this.currentRound = new Round(deck)
+  }
+
+  start() {
+    this.createGameDeck(prototypeQuestions)
+    this.printMessage(this.currentRound.deck, this.currentRound)
+    this.printQuestion(this.currentRound)
+  }
+
 }
 
 module.exports = Game;
---
+++ test/Game-test.js
@@ -0,0 +1,50 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const Card = require("../src/Card");
+const Deck = require("../src/Deck");
+const Game = require("../src/Game");
+const Round = require("../src/Round")
+
+describe("Game", function () {
+  let cards, deck, round, cardData;
+
+  beforeEach(() => {
+    cardData = [{
+        "id": 5,
+        "question": "What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?",
+        "answers": ["mutator method", "accessor method", "iteration method"],
+        "correctAnswer": "iteration method"
+      }, {
+        "id": 6,
+        "question": "What is an example of a mutator method?",
+        "answers": ["sort()", "map()", "join()"],
+        "correctAnswer": "sort()"
+      }, {
+            "id": 20,
+            "question": "forEach() returns an array",
+            "answers": ["true", "false"],
+            "correctAnswer": "false"
+          }
+      ]
+    cards = cardData.map(card => {
+      return new Card(card.id, card.question, card.answers, card.correctAnswer)
+    })
+    deck = new Deck(cards);
+    round = new Round(deck);
+    game = new Game()
+  });
+
+  it("should be a function", function () {
+    expect(Game).to.be.a("function");
+  });
+
+  it("should be an instance of Game", function () {
+    expect(game).to.be.an.instanceof(Game);
+  });
+
+  it("should keep track of the current round", function () {
+    game.createGameDeck(cardData)
+    expect(game.currentRound).to.deep.equal(round);
+  });
+});
